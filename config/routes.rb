610b01e4ee4c86de4da45537e35926d7d4ad802e
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-12-06 18:58:29 +0000

    board index matches target

--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,8 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #Root URL
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get("/", { :controller => "boards", :action => "index" })
+
 end
